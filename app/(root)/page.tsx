7ca04f81a84ce4d4b642244557fa0db3850fa1c0
fedkin@bramblingtech.com <fedkin@bramblingtech.com>
2024-05-05 11:18:07 +0300

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,41 @@
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: 'Anf',
+    lastName: 'AAA',
+    email: 'anf17@gmail.com',
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 173.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
